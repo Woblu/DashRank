7c3a793ae5365901ed3d76e50a01d7b4298cace9
Woblu <boaz.vega@proton.me>
2025-10-02 17:48:52 -0500

    Home.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
-// src/main.jsx
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import { AuthProvider } from "./contexts/AuthContext.jsx"; // Import AuthProvider
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import App from './App.jsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider> {/* Add the provider here */}
-      <App />
-    </AuthProvider>
-  </React.StrictMode>
+    {/* BrowserRouter must wrap AuthProvider */}
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
 );
